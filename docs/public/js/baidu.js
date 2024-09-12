30ab79bedc048dc9e47ae5029bef23dab117accf
Yanjin_Li <yanjinli502@gmail.com>
2024-09-12 22:55:35 +0800

    textbook uploaded

--- docs/public/js/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?SOME_ID";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
